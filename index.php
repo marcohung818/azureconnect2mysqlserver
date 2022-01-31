431cd9d29f9beef3e421e8558b48e519783f2131
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2022-01-31 15:29:45 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php 
+    
+  $host = 'ap-cloud-quiz-mysql.mysql.database.azure.com';
+  $username = 'azuresqladmin@ap-cloud-quiz-mysql';
+  $password = '#Marco121521';
+  $db_name = 'ap-cloud-quiz-mysqldb';
+
+  //Initializes MySQLi
+  $conn = mysqli_init();
+
+  mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+  // Establish the connection
+  mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin@mydemoserver', 'yourpassword', 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+  //If connection failed, show the error
+  if (mysqli_connect_errno())
+  {
+      die('Failed to connect to MySQL: '.mysqli_connect_error());
+  }
+    
+    
+    
+    
+?> 
