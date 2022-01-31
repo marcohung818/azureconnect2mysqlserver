64e2236a87f5ea5c1372cafda530c69a1ebc48ab
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2022-01-31 16:07:46 +0000

    establish connection

--- index.php
+++ index.php
@@ -12,15 +12,11 @@
   mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
   // Establish the connection
-  mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin@mydemoserver', 'yourpassword', 'quickstartdb', 3306, NULL, MYSQLI_CLIENT_SSL);
+  mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
   //If connection failed, show the error
   if (mysqli_connect_errno())
   {
       die('Failed to connect to MySQL: '.mysqli_connect_error());
   }
-    
-    
-    
-    
 ?> 
